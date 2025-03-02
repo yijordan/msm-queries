12c7fab5e9cc4c06eb5962a351798cbd89166543
yijordan <yij@uchicago.edu>
2025-03-02 23:41:40 +0000

    working on role table

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -4,7 +4,8 @@ class ActorsController < ApplicationController
   end
   def show
     @the_id = params.fetch("the_id")
-    @the_actor = Actor.where({ })
+    @the_actor = Actor.where({ :id => @the_id }).at(0)
+    @roles = Character.where({ :actor_id => @the_id})
     render({ :template => "actor_templates/details" })
   end
 end
