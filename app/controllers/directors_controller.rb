ff6593b5b6805ef199be1418d8883ad89c574091
yijordan <yij@uchicago.edu>
2025-03-02 19:16:03 +0000

    holy shit i figured out the details page!!!! all on my own!

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -4,6 +4,8 @@ class DirectorsController < ApplicationController
   end
   def show
     @the_id = params.fetch("the_id")
+    @the_director = Director.where({ :id => @the_id }).at(0)
+    @directors_movies = Movie.where({ :director_id => @the_id})
     render({ :template => "director_templates/details"})
   end
 end
