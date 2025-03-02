457eeecfbc894c07615123ae895630ca26db8de7
yijordan <yij@uchicago.edu>
2025-03-02 19:56:21 +0000

    cleaned it up a bit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,8 +5,7 @@ class MoviesController < ApplicationController
   def show
     @the_id = params.fetch("the_id")
     @the_movie = Movie.where({ :id => @the_id }).at(0)
-    @movies_director = @the_movie.director_id
-    @movies_directors_name = Director.where({ :id => @movies_director}).at(0).name
+    @movies_director = Director.where({ :id => @the_movie.director_id}).at(0).name
     render({ :template => "movie_templates/details"})
 
   end
