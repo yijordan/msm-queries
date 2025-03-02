6f2cf822ab44a25f5f95553859409a94b178d357
yijordan <yij@uchicago.edu>
2025-03-02 19:52:04 +0000

    finally figured out the movies directors name

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,12 @@ class MoviesController < ApplicationController
   def index
     render({ :template => "movie_templates/list"})
   end
+  def show
+    @the_id = params.fetch("the_id")
+    @the_movie = Movie.where({ :id => @the_id }).at(0)
+    @movies_director = @the_movie.director_id
+    @movies_directors_name = Director.where({ :id => @movies_director}).at(0).name
+    render({ :template => "movie_templates/details"})
+
+  end
 end
