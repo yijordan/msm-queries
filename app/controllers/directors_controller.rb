c6b2e08b4bb7a3edf07c1d740e28ce48e4e45d96
yijordan <yij@uchicago.edu>
2025-03-02 19:05:22 +0000

    Working on the details page,, fixed my cursor issues

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,6 +3,7 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/list"})
   end
   def show
+    @the_id = params.fetch("the_id")
     render({ :template => "director_templates/details"})
   end
 end
