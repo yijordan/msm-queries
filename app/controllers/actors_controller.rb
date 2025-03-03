a2cef321771a2faa8d1645967957d5103d2233d2
yijordan <yij@uchicago.edu>
2025-03-03 00:20:50 +0000

    fiiiinally fixed the roles each

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -5,8 +5,7 @@ class ActorsController < ApplicationController
   def show
     @the_id = params.fetch("the_id")
     @the_actor = Actor.where({ :id => @the_id }).at(0)
-    @roles = Character.where({ :actor_id => @the_id}).at(0)
-    @actors_movies = Movie.where({ :id => @roles.movie_id })
+    @roles = Character.where({ :actor_id => @the_id})
     render({ :template => "actor_templates/details" })
   end
 end
